f732a666af29c95c2f60b90f015351afc0394a7f
Wyatt J. Miller <wyatt@wyattjmiller.com>
2022-07-29 18:18:24 -0400

    it works

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibGit2Sharp;
+
+static class Public
+{
+    internal static void Main(string[] args)
+    {
+        List<string> authors = new List<string>();
+        Dictionary<string, int> commitReport = new Dictionary<string, int>();
+
+        using (var repo = new Repository(@"."))
+        {
+            foreach (var c in repo.Commits)
+            {
+                if (!authors.Contains(c.Author.Email))
+                {
+                    authors.Add(c.Author.Email);
+                }
+            }
+            
+            foreach (var a in authors)
+            {
+                int commitCount = repo.Commits.OrderBy(a => a.Author).Count();
+                commitReport.Add(a, commitCount);
+            }
+
+        }
+
+        foreach (var r in commitReport)
+        {
+            System.Console.WriteLine($"{r.ToString()}");
+        }
+    }
+}
